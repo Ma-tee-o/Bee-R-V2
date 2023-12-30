085c068830ac46716d2ec1f023fcd02a311b852d
mariatei <maria.te@gmx.net>
2023-12-30 13:07:01 +0100

    Bee Vision implementet on Y Button Press

--- Assets/Scripts/BeeVision.cs
+++ Assets/Scripts/BeeVision.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using Unity.XR.CoreUtils;
 
 public class BeeVision : MonoBehaviour
 {
+    public Camera defaultVision;
+    public Camera BeeVisionCam;
+    public InputDeviceCharacteristics inputDeviceCharacteristics;
+    
+    public InputDevice _targetDevice;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        BeeVisionCam.enabled = false;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(inputDeviceCharacteristics, devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+            Debug.Log(devices[0].name + devices[0].characteristics);
+            _targetDevice = devices[0];
+        }
         
+
+        if (_targetDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool triggerValue) && triggerValue)
+        {
+            Debug.Log("Y button is pressed.");
+
+            defaultVision.enabled = !defaultVision.enabled;
+            BeeVisionCam.enabled = !BeeVisionCam.enabled;
+
+        }
     }
+
 }
+    
+    
+
+      
+    
---
+++ Assets/Scripts/DeviceManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class DeviceManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevices(devices);
+
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+    }
+}
